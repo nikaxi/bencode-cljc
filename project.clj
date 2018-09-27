635f7d5000526a478cd39d1f3f20f966d51bc3b1
James Leonis <me@jamesleonis.com>
2018-09-26 19:41:25 -0700

    Begin migration to deps.edn

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject com.jamesleonis/bencode-cljc "0.1.3-SNAPSHOT"
+
   :description "A functional Clojure(script) BEncode serialization library."
   :url "https://github.com/jamesleonis/bencode-cljc"
   :license {:name "Eclipse Public License - v 2.0"
             :url "https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.txt"}
 
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.946" :scope "provided"]]
+                 [org.clojure/clojurescript "1.10.339" :scope "provided"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]]
 
